1721d87b48da49b711a901c81f4e5c300463426d
Tom Dooner <tom.dooner@brigade.com>
2017-03-28 17:25:02 -0700

    Use actual institutional counselor name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
                         User.find(session[:user_id])
                       end
   end
+
+  # Reorders a name if it contains a comma. E.g. given "Smith, Joe" will return
+  # "Joe Smith"
+  def name_reorderer(full_name)
+    return full_name unless full_name.include?(',')
+
+    last, first = full_name.split(',', 2)
+
+    "#{first} #{last}".chomp
+  end
+
+  # Names are hard, this method assumes Western naming conventions.
+  def first_name(full_name)
+    (full_name.include?(',') ? name_reorderer(full_name) : full_name).split.first
+  end
 end
