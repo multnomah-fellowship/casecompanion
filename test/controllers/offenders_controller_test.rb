a75e0046c1ce1cf786e939d6ead2754bb7ebe65b
Tom Dooner <tom.dooner@brigade.com>
2017-03-30 11:53:41 -0700

    Remove test_unit tests
    Viva la rspec.

--- test/controllers/offenders_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OffendersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get offenders_show_url
-    assert_response :success
-  end
-
-end
