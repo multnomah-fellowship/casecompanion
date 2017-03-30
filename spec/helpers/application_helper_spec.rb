82174774baff3ff145a05e8e7fc2e8b08d4b8bef
Tom Dooner <tom.dooner@brigade.com>
2017-03-30 11:56:01 -0700

    Add test coverage for #format_phone

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,4 +13,10 @@ describe ApplicationHelper do
       expect(helper.first_name('Laurel Calaway')).to eq('Laurel')
     end
   end
+
+  describe '#format_phone' do
+    it 'formats a phone number as intended' do
+      expect(helper.format_phone('+11234567890')).to eq('(123) 456-7890')
+    end
+  end
 end
