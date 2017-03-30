a1d482746386ade8e7371a387a680dc84c1491ce
Tom Dooner <tom.dooner@brigade.com>
2017-03-30 11:52:00 -0700

    Add some test coverage for HomeController
    These don't fully work yet, I need to rebase this branch first.

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe HomeController do
+  render_views
+
+  describe '#index' do
+    let(:session) { {} }
+    subject { get :index, session: session }
+
+    describe 'when logged in as a victim advocate' do
+      let(:user) { User.create!(email: 'foo@bar.com', password: 'foobar') }
+
+      it 'renders links to send a notification' do
+        session[:user_id] = user.id
+        subject
+        expect(response).to be_success
+        expect(response.body).to include('Send Invitation')
+      end
+    end
+
+    describe 'with a notification_id in the session' do
+      let(:offender_sid) { (Random.rand * 100_000_000).floor }
+      let(:notification) do
+        Notification.create!(
+          first_name: 'Tom',
+          offender_sid: offender_sid,
+          phone_number: '123-456-7890'
+        )
+      end
+
+      it 'renders a link directly to that offender show page' do
+        session[:notification_id] = notification.id
+        subject
+        expect(response).to be_success
+        doc = Nokogiri::HTML(response.body)
+        link = doc.css("a[href=\"/offenders/#{offender_sid}\"]")
+        expect(link).to be_present
+      end
+    end
+
+    describe 'when logged out' do
+      it 'renders a link to the offender search page' do
+        subject
+        expect(response).to be_success
+        doc = Nokogiri::HTML(response.body)
+        link = doc.css("a[href=\"/offenders\"]")
+        expect(link).to be_present
+      end
+    end
+  end
+
+  describe '#notification_systems' do
+    subject { get :notification_systems }
+
+    it 'renders' do
+      subject
+      expect(response).to be_success
+    end
+  end
+end
