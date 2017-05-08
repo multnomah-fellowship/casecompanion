63ebdced5893757c2d6dae70457143a7a510c958
Tom Dooner <tom.dooner@brigade.com>
2017-05-08 16:13:20 -0700

    Fix domain name in sitemap host
    The "www" must be omitted.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.myadvocateoregon.org"
+SitemapGenerator::Sitemap.default_host = "https://myadvocateoregon.org"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
