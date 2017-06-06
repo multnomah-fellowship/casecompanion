ecd5f54dc8050b3fce78b89a7932c53efce17437
Tom Dooner <tdooner@codeforamerica.org>
2017-06-06 15:47:39 -0700

    Add DCJ Baxter API client
    This is a simple client for the DCJ baxter API as a starting point for
    getting probation data in the system. Still much to do:
    
    1. Switch over to the production Baxter API (once it is deployed)
    2. Request an API change to make the lastName argument not required if
       searching by SID.
    3. Add caching because this is gonna be slooooooow.
    4. Figure out how to handle searching across both databases.

---
+++ app/lib/dcj_client.rb
@@ -0,0 +1,43 @@
+module DcjClient
+  # TODO: replace this with the production one once the key is deployed
+  URL_BASE = URI('https://uat.multco.us')
+
+  class RequestError < StandardError; end
+
+  def initialize(api_key: ENV['DCJ_BAXTER_API_KEY'])
+    @api_key = api_key
+  end
+
+  # For now we have to search with the last name as well.
+  #
+  # TODO: Add caching, see if we can do a search without a last name.
+  #
+  # @return Hash? Information about the offender, if found.
+  def self.offender_details(sid:, last_name:)
+    fetch_offender_details(sid: sid, last_name: last_name)
+  end
+
+  private
+
+  def self.fetch_offender_details(sid:, last_name:)
+    Net::HTTP.start(URL_BASE.host, URL_BASE.port) do |http|
+      request_uri = '/Baxter/api/polookup?' + URI.encode_www_form(
+        key: @api_key,
+        sid: sid,
+        lastName: last_name
+      )
+
+      req = Net::HTTP::Get.new(request_uri)
+      req['Accept'] = 'application/json'
+
+      response = http.request(req)
+      body = JSON.parse(response.body)
+
+      unless response.code.to_i < 300
+        raise RequestError.new("Baxter API Request Failed: #{body['Message']}")
+      end
+
+      body
+    end
+  end
+end
---
+++ spec/lib/dcj_client_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe DcjClient do
+  let(:offender_hash) do
+    {
+      'OffenderFirstName': 'John',
+      'OffenderLastName': 'Wilhite',
+      'SID': 20130142,
+      'DOB': '1977-11-07T00:00:00',
+      'POFirstName': 'Frank',
+      'POLastName': 'SomeRandomName',
+      'POPhone': '503-555-1234 ext 12345',
+    }
+  end
+
+  before do
+    allow(DcjClient).to receive(:fetch_offender_details)
+      .and_return(offender_hash)
+  end
+
+  describe '#offender_details' do
+    subject { described_class.offender_details(last_name: 'foo', sid: 1234) }
+
+    it 'returns a hash of data' do
+      expect(subject).to be_a(Hash)
+    end
+  end
+end
