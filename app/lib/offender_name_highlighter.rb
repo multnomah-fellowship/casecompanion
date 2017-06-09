3d65c4d8df3bc90fd31b7e9dc9628ecaca7d2295
Tom Dooner <tdooner@codeforamerica.org>
2017-06-09 15:27:31 -0700

    Fix OffenderNameHighlighter to handle incomplete queries

--- app/lib/offender_name_highlighter.rb
+++ app/lib/offender_name_highlighter.rb
@@ -1,7 +1,7 @@
 class OffenderNameHighlighter
   include ActionView::Helpers::SanitizeHelper
 
-  HIGHLIGHT_FIELD_NAMES = %i[first_name last_name]
+  HIGHLIGHT_FIELD_NAMES = %i[first_name last_name dcj_last_name]
 
   def initialize(search_params)
     @search_params = search_params
@@ -10,6 +10,7 @@ class OffenderNameHighlighter
   def highlight(text)
     text = sanitize(text)
     regex_str = @search_params.values_at(*HIGHLIGHT_FIELD_NAMES)
+      .compact
       .map { |val| Regexp.escape(val) }
       .join('|')
     text.gsub!(/(\b)(#{regex_str})(\b)/i, '\1<strong>\2</strong>\3')
