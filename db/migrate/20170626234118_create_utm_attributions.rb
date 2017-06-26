4bcbfc06b2546c4ca99ee0bd9dc8153bd7a9ae85
Tom Dooner <tdooner@codeforamerica.org>
2017-06-26 16:42:54 -0700

    Create UtmAttribution model
    This is the beginning of being able to track when a user completes an
    action having entered the site with attribution parameters.
    
    I'm not sure what this will look like in the frontend, but I'm thinking
    of perhaps redirecting from ?utm_campaign=something to a URL without the
    attribution parameters and storing the attribution ID in the session.

---
+++ db/migrate/20170626234118_create_utm_attributions.rb
@@ -0,0 +1,12 @@
+class CreateUtmAttributions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :utm_attributions do |t|
+      t.string :utm_source
+      t.string :utm_content
+      t.string :utm_medium
+      t.string :utm_campaign
+
+      t.timestamps
+    end
+  end
+end
