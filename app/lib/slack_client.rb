bb092b643150c92f3c0af14851052af57bcda021
Tom Dooner <tdooner@codeforamerica.org>
2017-07-05 17:18:09 -0700

    Send beta signup notifications to Slack
    When a user signs up for our beta, we should be notified so we don't
    need to check the database constantly.
    
    Closes https://github.com/multnomah-fellowship/planning/issues/158

--- app/lib/slack_client.rb
+++ app/lib/slack_client.rb
@@ -4,6 +4,38 @@ class SlackClient
     @app_name = app_name
   end
 
+  def post_beta_signup_message(beta_signup)
+    return { error: 'No Webhook Specified' } unless @hook_url.present?
+
+    attribution_fields =
+      beta_signup.utm_attribution
+        .try(:slice, *UtmAttribution::FIELDS)
+        .try(:compact)
+        .try(:map) { |k, v| { title: k, value: v, short: true } }
+        .to_a
+
+    beta_signup_attachment = {
+      attachments: [
+        {
+          fallback: "New beta signup: #{beta_signup.email}",
+          color: '#2f6296', # color: multnomah-blue
+          pretext: 'New Beta Signup!',
+          fields: [
+            {
+              title: 'Email',
+              value: beta_signup.email,
+              short: true
+            },
+          ] + attribution_fields,
+          footer: @app_name,
+          ts: beta_signup.created_at.to_i,
+        }
+      ]
+    }
+
+    send_message(beta_signup_attachment)
+  end
+
   def post_feedback_message(feedback)
     return { error: 'No Webhook Specified' } unless @hook_url.present?
 
@@ -32,11 +64,17 @@ class SlackClient
       ]
     }
 
+    send_message(feedback_attachment)
+  end
+
+  private
+
+  def send_message(attachment)
     slack_uri = URI(@hook_url)
     Net::HTTP.start(slack_uri.host, slack_uri.port, use_ssl: true) do |http|
       req = Net::HTTP::Post.new(slack_uri.request_uri)
       req['Content-Type'] = 'application/json'
-      req.body = JSON.generate(feedback_attachment)
+      req.body = JSON.generate(attachment)
 
       resp = http.request(req)
       if resp.code.to_i < 300
