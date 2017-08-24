ac07ea9723aac6c1a50d83acd099ff246cd2f2a5
Tom Dooner <tdooner@codeforamerica.org>
2017-08-23 22:18:36 -0700

    Make FeedbackResponse `utm_attribution` optional by default
    Not every FeedbackResponse needs to have a UTM attribution.

--- app/models/feedback_response.rb
+++ app/models/feedback_response.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FeedbackResponse < ApplicationRecord
-  belongs_to :utm_attribution
+  belongs_to :utm_attribution, optional: true
   accepts_nested_attributes_for :utm_attribution
 
   enum value: %i[thumbs_up thumbs_down yes_but]
