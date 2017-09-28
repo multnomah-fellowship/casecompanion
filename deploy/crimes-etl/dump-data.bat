daab2faf50ac585e99fc35625a66e4391156fdc0
Tom Dooner <tdooner@codeforamerica.org>
2017-09-27 17:30:24 -0700

    Split DCJ Email query into a couple temp tables
    This query was getting too gnarly to debug, so I've broken it down into
    a couple temp tables: one for each of probation and restitution
    sentences, and then one for the victims and one for the defendants in
    those cases.

--- deploy/crimes-etl/dump-data.bat
+++ deploy/crimes-etl/dump-data.bat
@@ -9,7 +9,7 @@ cd C:\Users\doonert\casecompanion\deploy\crimes-etl
 
 sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\00_SETUP.sql
 
-sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\01_VRN.sql
+sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\01_TMP_TABLES.sql
 
 sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\q-before.sql,queries\02_VICTIM_NAME.sql -o victims.csv -s "^" -W
 
@@ -17,6 +17,13 @@ sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\q-before.s
 
 sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\q-before.sql,queries\04_CASE_INFO.sql -o cases.csv -s "^" -W
 
-sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -i queries\q-before.sql,queries\05_DCJ_EMAIL.sql -o closed_charge_victims.csv -s "^" -W
+sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -Q "SET NOCOUNT ON; SELECT * FROM CFA_TOM_DEFENDANT_INFO" -o defendants.csv -s "^" -W
+
+REM TODO: All victims vs victims?
+sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -Q "SET NOCOUNT ON; SELECT * FROM CFA_TOM_VICTIM_INFO" -o all_victims.csv -s "^" -W
+
+sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -Q "SET NOCOUNT ON; SELECT * FROM CFA_TOM_PROBATION" -o probation_sentences.csv -s "^" -W
+
+sqlcmd -S "dacry2.mcda.mccj.local\mssql$dada2" -d Crimsadl -Q "SET NOCOUNT ON; SELECT * FROM CFA_TOM_RESTITUTION" -o restitution_sentences.csv -s "^" -W
 
 pause
