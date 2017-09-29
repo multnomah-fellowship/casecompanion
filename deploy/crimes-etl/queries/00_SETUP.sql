490fa715dcba33bcd33e5291317b27f870f38344
Tom Dooner <tdooner@codeforamerica.org>
2017-09-28 18:07:45 -0700

    Add CREATE INDEX commands to the SETUP
    These indices take only a few seconds to create, each, and not much disk
    space, but shave minutes off the subsequent queries. I'm sure these
    indexes exist in the Oracle CRIMES prod database, but just aren't getting
    replicated over.

--- deploy/crimes-etl/queries/00_SETUP.sql
+++ deploy/crimes-etl/queries/00_SETUP.sql
@@ -4,8 +4,22 @@ DROP TABLE CFA_TOM_RESTITUTION;
 DROP TABLE CFA_TOM_VICTIM_INFO;
 DROP TABLE CFA_TOM_DEFENDANT_INFO;
 
--- TODO: These queries will all run faster with the following indices:
--- CREATE INDEX cfa_tom_case_person ON CASE_PERSON (CASE_ID_NBR, CASE_PERSON_TYPE, PERSON_ID_NBR);
--- CREATE INDEX cfa_tom_cp_address ON CASE_PERSON_ADDRESS (PERSON_ID_NBR, SUB_ONLY, ALL_DOCS) WITH DROP_EXISTING;
--- CREATE INDEX cfa_tom_cp_phone ON CASE_PERSON_PHONE (PERSON_ID_NBR, DEFAULT_PHONE) WITH DROP_EXISTING;
--- CREATE INDEX cfa_tom_cp_info ON CASE_PERSON_INFO (PERSON_ID_NBR);
+-- These queries will all run faster with the following indices:
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_case_person') BEGIN
+CREATE INDEX cfa_tom_case_person ON CASE_PERSON (CASE_ID_NBR, CASE_PERSON_TYPE, PERSON_ID_NBR);
+END
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_cp_address') BEGIN
+CREATE INDEX cfa_tom_cp_address ON CASE_PERSON_ADDRESS (PERSON_ID_NBR, SUB_ONLY, ALL_DOCS) WITH DROP_EXISTING;
+END
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_cp_phone') BEGIN
+CREATE INDEX cfa_tom_cp_phone ON CASE_PERSON_PHONE (PERSON_ID_NBR, DEFAULT_PHONE) WITH DROP_EXISTING;
+END
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_cp_info') BEGIN
+CREATE INDEX cfa_tom_cp_info ON CASE_PERSON_INFO (PERSON_ID_NBR);
+END
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_case_main') BEGIN
+CREATE INDEX cfa_tom_case_main ON CASE_MAIN (CASE_ID_NBR);
+END
+IF NOT EXISTS(SELECT * FROM sysindexes where name = 'cfa_tom_cp_flag') BEGIN
+CREATE INDEX cfa_tom_cp_flag ON CASE_FLAG (CASE_ID_NBR, PERSON_ID_NBR);
+END
