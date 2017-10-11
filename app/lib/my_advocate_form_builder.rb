82df0066fd75ca71b3766e782951b51dbb872ec2
Tom Dooner <tdooner@codeforamerica.org>
2017-10-10 17:48:24 -0700

    Update layout to new grid system
    The new grid system is somewhat confusing -- the designs will align to a
    large fixed grid (called the "outer grid") in the comments here, but in
    practice implementing this grid is not possible with materialize's
    dynamic percentages because they multiply together in ways that make it
    impossible to say "make this <input> 4-outer-columns wide" inside the
    inner grid.
    
    So, this creates just a container for the inner grid instead of trying
    to make an outer grid too.
    
    While restyling everything, this also creates new row styling with more
    padding:
    
    - the `app-row` class has 60px of top/bottom margin
    - the `app-row--large` class has 80px of top/bottom margin
      (margin is used so that consecutive rows' margins can collapse)
    - the `app-row--with-separator` class adds a dividing line
    
    There were a couple other `app-row` variants, and I've updated them to
    fit this scheme as well.

--- app/lib/my_advocate_form_builder.rb
+++ app/lib/my_advocate_form_builder.rb
@@ -38,16 +38,15 @@ class MyAdvocateFormBuilder < ActionView::Helpers::FormBuilder
   def dob_select_field(method)
     fields_for(method) do |f|
       <<-HTML.html_safe
-        <div class='row'>
-          <div class='col s10 offset-s1'>
+          <div class='col s12'>
             #{f.label(:month, 'Date of Birth')}
           </div>
-          <div class='col s3 offset-s1 m1 offset-m1'>
+          <div class='col s3 m2'>
             <div class='input-field app-input-field--no-label'>
               #{f.text_field(:month, placeholder: 'MM')}
             </div>
           </div>
-          <div class='col s3 m1'>
+          <div class='col s3 m2'>
             <div class='input-field app-input-field--no-label'>
               #{f.text_field(:day, placeholder: 'DD')}
             </div>
