867472d82fdeb6e4120115e057b45dadacc5ccef
Tom Dooner <tdooner@codeforamerica.org>
2017-10-12 12:31:14 -0700

    Attempt to rein-in the gnarly Haml backtraces
    Haml creates backtraces that don't dedupe properly in Sentry due to
    having dynamically-generated filenames like the line:
    
    ```
    from app/views/home/home.html.haml:15:in `block in
    _app_views_home_home_html_haml___3281773964777071596_47325545592280'
    ```
    
    By removing the numbers, hopefully we can make all errors from this line
    become a single error in Sentry.

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -4,6 +4,7 @@
 # You can add backtrace silencers for libraries that you're using but don't wish
 # to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+Rails.backtrace_cleaner.add_filter { |line| line.gsub(/haml__(\d)+_(\d)+/, 'haml') }
 
 # You can also remove all the silencers if you're trying to debug a problem that
 # might stem from framework code.
