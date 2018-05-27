5c855734b9669c230f88c7b291a9f27bb676ef84
Tom Dooner <tomdooner@gmail.com>
2018-05-26 22:06:18 -0700

    Add some better debug information to the debug page

--- app/controllers/admin/import_statuses_controller.rb
+++ app/controllers/admin/import_statuses_controller.rb
@@ -4,16 +4,43 @@ class Admin::ImportStatusesController < ApplicationController
   before_action :require_admin!
 
   def show
+    @revision = load_revision
     @lines = logger_lines
+    @deploy_time = deploy_time
   end
 
   private
-  
+
+  def load_revision
+    File.read(Rails.root.join('REVISION'))
+  rescue
+    return 'master'
+  end
+
   def logger_lines
     SqlTableLogger
       .new(ENV['LOCAL_ATTORNEY_MANAGER_DATABASE_URL'], 'attorney_manager_import_log')
       .lines
   rescue
     []
+  end
+
+  def deploy_time
+    root = Rails.root.basename.to_s
+    return 'Unknown' if root !~ /^\d+$/
+
+    year = root[0..3].to_i
+    month = root[4..5].to_i
+    day = root[6..7].to_i
+    hour = root[8..9].to_i
+    minute = root[10..11].to_i
+    second = root[12..13].to_i
+
+    DateTime
+      .new(year, month, day, hour, minute, second)
+      .localtime
+      .strftime('%A %B %-d, %Y at %-I:%M %P')
+  rescue
+    return 'Unknown'
   end
 end
--- lib/tasks/attorney_manager.rake
+++ lib/tasks/attorney_manager.rake
@@ -4,16 +4,18 @@ namespace :attorney_manager do
   task importer: :environment do
     @logger = SqlTableLogger.new(ENV['LOCAL_ATTORNEY_MANAGER_DATABASE_URL'], 'attorney_manager_import_log')
     @logger.reset!
-    @logger.capture_rails_logger do
-      @destination = LocalAttorneyManagerInPostgres.new
-      @importer = AttorneyManagerImporter.new(
-        destination: @destination
-      )
-    end
+    @destination = LocalAttorneyManagerInPostgres.new
+    @importer = AttorneyManagerImporter.new(
+      destination: @destination
+    )
   end
 
   desc 'Import all data from Attorney Manager into local database'
   task import_all: %i[importer] do
-    @importer.import_all
+    @logger.capture_rails_logger do
+      Rails.logger.info 'Beginning Attorney Manager import'
+      @importer.import_all
+      Rails.logger.info 'Finished Attorney Manager import successfully!'
+    end
   end
 end
